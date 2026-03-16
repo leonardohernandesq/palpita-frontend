d6235120c702a161fd749acb96c618c3a44bad3d
leonardohernandesq <leonardo_hernandes@outlook.com.br>
2026-03-16 23:27:04 +0000

    feat: configure husky

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  plugins: [
+    {
+      rules: {
+        "type-required": ({ header }) => {
+          const hasType = /^[a-z]+:/.test(header);
+
+          if (!hasType) {
+            return [
+              false,
+              "Your commit must follow: type: message (example: feat: add login page)",
+            ];
+          }
+
+          return [true];
+        },
+      },
+    },
+  ],
+  rules: {
+    "type-required": [2, "always"],
+    "type-empty": [0],
+    "subject-empty": [0],
+  },
 };
