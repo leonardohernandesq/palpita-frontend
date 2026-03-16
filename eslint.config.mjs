6cde4cf4657156b5e6b7655111866dd77b0c2fd1
leonardohernandesq <leonardo_hernandes@outlook.com.br>
2026-03-16 23:42:03 +0000

    feat: configure prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,14 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    plugins: ["prettier"],
+    extends: ["plugin:prettier/recommended"],
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
